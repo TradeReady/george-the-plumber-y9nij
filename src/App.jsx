be1b8e8cf04fe350eb3401c5e1e08bb387925e06
cbramall81 <cbramall81@gmail.com>
2026-05-13 13:17:55 +0100

    Update website: George The Plumber

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,9 @@ const site = {
   "call_notifications_enabled": false,
   "generated_content": {
     "benefits": [
-      "24/7 Emergency Service",
-      "Highly Experienced Technicians",
-      "Customer Satisfaction Guarant",
+      "24/7 Emergency Servic",
+      "Highly Experienced Technician",
+      "Customer Satisfaction Guarantee",
       "Transparent Pricin",
       "Wide Range of Plumbing Services"
     ],
@@ -110,7 +110,7 @@ const site = {
   "status": "viewed",
   "id": "69ddfba9ce6b446ef142b025",
   "created_date": "2026-04-14T08:32:41.207000",
-  "updated_date": "2026-05-13T12:15:32.071000",
+  "updated_date": "2026-05-13T12:17:38.435000",
   "created_by_id": "69dd00d82d4b211b7a1d51c1",
   "created_by": "cb.base44@gmail.com",
   "is_sample": false
